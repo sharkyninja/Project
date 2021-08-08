b6fd79f2d42de0a96b4bfbf1aec5c2f84d8b9320
Glow-Squid <thecoolersquid@gmail.com>
2021-08-08 09:07:52 +0400

    Update project.py

--- project.py
+++ project.py
@@ -1,26 +1,60 @@
 print("Importing Libraries..")
-import os
-import mysql.connector as myconn
-import matplotlib.pyplot as pl
+import os 
+import random
 import pandas as pd
-from Userdata import all_user
-print("Connecting to MySQL...")
-con = myconn.connect(user="root", passwd="12345678", database="project")
-if con.is_connected():
-    print("MySQL:Connected to the Database")
+import matplotlib.pyplot as pl
+import mysql.connector as myconn
+db=open("Users.txt","r")
+print("MySQL:Connecting to Database")
+sql=myconn.connect(host="localhost",user="root",passwd="12345678",database="project")
+mycon=sql.cursor()
+if sql.is_connected():
+    print("MySQL: Connected")
 else:
-    print("MySQL:Connection Error")
+    print("MySQL: Unable to connect")
 
-def signup():
-    print("Create a User Id:")
-    nuser=input()
-    print("Create Password:")
-    Npass=input()
-    print("Confirm Password:")
-    npass1=input()
-    
-    
-def post_login():
-    print("Welcome {user}!")
+def Startup():
+    print("Welcome")
+    print("1.Login\n2.Signup")
+    o=int(input())
+    return(0)
+def Signup():
+    print("******Signup******")
+    print("Create a User_name")
+    n_user=input()
+    print("Create Password")
+    n_passwd=input()
+    print("Confirm Password")
+    c_passwd=input()
+    if len(n_passwd)<8:
+        print("Password must contain atleast 8 characters.")
+    if n_passwd!=c_passwd:
+        print("Passwords don't match.")
+    else:
+        dbe=open("Users.txt","a")
+        dbe.write(f"{n_user},{n_passwd}\n")
+        create_table=(f"create table {n_user}(Name varchar(25),Type_1 varchar(20),Type_2 varchar(20),Total int,Health_Points int,Attack int,Defense int,Special_Attack int,Special_defense int,Speed int,Generation int,Legendery char(5));")
+        mycon.execute(create_table)
+        sql.commit()
+        print("Successfuly created an account.")
+def Login():
+    print("*******Login*******")
+    print("Enter Username")
+    user=input()
+    print("Enter Password")
+    passwd=input()
+
+    return(user)
+def Main():
+    print("welcome,{user}")
     print("Choose an option from below to Continue:")
-    print("1.Take a Draw for pokemones")
+    print("1.Take a Draw for pokemones.")
+    print("2.View all your pokemones.")#m_stephin
+    print("3.Match.")
+    print("4.Check my balance.")#JJ
+def random_poki():
+    print("Your Coins:{coins}")
+    print("Cost per draw: 1 coin")
+    print("Enter the number of draw u would like to take.")
+    x=int(input())
+    return(x)
